88980fcbc0f089048dfca98cdd17740d28bc7863
tr7zw <tr7zw@live.de>
2023-01-27 20:31:04 +0100

    Update patch forks Github ci files

--- src/main/java/dev/tr7zw/gradle_compose/App.java
+++ src/main/java/dev/tr7zw/gradle_compose/App.java
@@ -58,40 +58,63 @@ public class App {
         FileUtil.copyIfAvailable(provider, "gradle/wrapper/gradle-wrapper.jar");
         Map<String, String> baseReplacements = FileProcessingUtil.mergeReplacements(data.replacements,
                 template.defaultReplacements);
-        updateProject(new File("."), baseReplacements, provider, data.rootProject, template.availableFlags, data.enabledFlags);
+        updateProject(new File("."), baseReplacements, provider, data.rootProject, template.availableFlags,
+                data.enabledFlags);
         for (Entry<String, Project> entry : data.subProjects.entrySet()) {
-            updateProject(new File(".", entry.getKey()), baseReplacements, provider, entry.getValue(), template.availableFlags, data.enabledFlags);
+            updateProject(new File(".", entry.getKey()), baseReplacements, provider, entry.getValue(),
+                    template.availableFlags, data.enabledFlags);
         }
         if (data.version.equals("0.0.1")) {
             Set<String> customEntries = ConfigUtil.readCustomList(provider, "custom.compose");
             for (String name : customEntries) {
                 FileUtil.copyIfAvailableWithReplacments(provider, new File("."), data.rootProject.template, name,
-                        FileProcessingUtil.mergeReplacements(data.rootProject.replacements, baseReplacements), template.availableFlags, data.enabledFlags);
+                        FileProcessingUtil.mergeReplacements(data.rootProject.replacements, baseReplacements),
+                        template.availableFlags, data.enabledFlags);
             }
         } else {
             for (String name : template.customEntries) {
                 FileUtil.copyIfAvailableWithReplacments(provider, new File("."), data.rootProject.template, name,
-                        FileProcessingUtil.mergeReplacements(data.rootProject.replacements, baseReplacements), template.availableFlags, data.enabledFlags);
+                        FileProcessingUtil.mergeReplacements(data.rootProject.replacements, baseReplacements),
+                        template.availableFlags, data.enabledFlags);
             }
         }
+        processFork(baseReplacements, provider, data.rootProject, template.availableFlags, data.enabledFlags);
     }
 
-    private void updateProject(File baseDir, Map<String, String> replacements, SourceProvider provider,
-            Project project, Set<String> availableTags, Set<String> enabledTags) {
+    private void updateProject(File baseDir, Map<String, String> replacements, SourceProvider provider, Project project,
+            Set<String> availableTags, Set<String> enabledTags) {
         Map<String, String> projectReplacements = FileProcessingUtil.mergeReplacements(project.replacements,
                 replacements);
-        FileUtil.copyIfAvailableWithReplacments(provider, baseDir, project.template, "github/workflows/build.yml", ".github/workflows/build.yml",
-                projectReplacements, availableTags, enabledTags);
-        FileUtil.copyIfAvailableWithReplacments(provider, baseDir, project.template, "github/workflows/tag.yml", ".github/workflows/tag.yml",
-                projectReplacements, availableTags, enabledTags);
-        FileUtil.copyIfAvailableWithReplacments(provider, baseDir, project.template, "github/FUNDING.yml", ".github/FUNDING.yml",
-                projectReplacements, availableTags, enabledTags);
+        FileUtil.copyIfAvailableWithReplacments(provider, baseDir, project.template, "github/workflows/build.yml",
+                ".github/workflows/build.yml", projectReplacements, availableTags, enabledTags);
+        FileUtil.copyIfAvailableWithReplacments(provider, baseDir, project.template, "github/workflows/tag.yml",
+                ".github/workflows/tag.yml", projectReplacements, availableTags, enabledTags);
+        FileUtil.copyIfAvailableWithReplacments(provider, baseDir, project.template, "github/FUNDING.yml",
+                ".github/FUNDING.yml", projectReplacements, availableTags, enabledTags);
         FileUtil.copyIfAvailableWithReplacments(provider, baseDir, project.template, "build.gradle",
                 projectReplacements, availableTags, enabledTags);
         FileUtil.copyIfAvailableWithReplacments(provider, baseDir, project.template, "gradle.properties",
                 projectReplacements, availableTags, enabledTags);
         FileUtil.copyIfAvailableWithReplacments(provider, baseDir, project.template, "settings.gradle",
                 projectReplacements, availableTags, enabledTags);
+    }
+
+    private void processFork(Map<String, String> replacements, SourceProvider provider, Project project,
+            Set<String> availableTags, Set<String> enabledTags) {
+        File parentDir = new File("..");
+        if (new File(parentDir, "patches").exists() && new File(parentDir, "upstream").exists()
+                && new File(parentDir, "workspace").exists() && new File(parentDir, "repo").exists()
+                && new File(parentDir, "sha").exists()) {
+            System.out.println("Updating fork .github folder...");
+            Map<String, String> projectReplacements = FileProcessingUtil.mergeReplacements(project.replacements,
+                    replacements);
+            FileUtil.copyIfAvailableWithReplacments(provider, parentDir, project.template, "github/forkworkflows/build.yml",
+                    ".github/workflows/build.yml", projectReplacements, availableTags, enabledTags);
+            FileUtil.copyIfAvailableWithReplacments(provider, parentDir, project.template, "github/forkworkflows/tag.yml",
+                    ".github/workflows/tag.yml", projectReplacements, availableTags, enabledTags); 
+            FileUtil.copyIfAvailableWithReplacments(provider, parentDir, project.template, "github/FUNDING.yml",
+                    ".github/FUNDING.yml", projectReplacements, availableTags, enabledTags);
+        }
     }
 
 }
